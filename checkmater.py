9ef3369c74b4444609e0c4de80510c84f0ae99ab
timothymullen <timothy.liam.mullen@gmail.com>
2016-07-11 01:06:19 -0400

    updated tile logic, added logging

--- checkmater.py
+++ checkmater.py
@@ -2,8 +2,13 @@ import chess
 import random
 import tweepy
 import pickle
+import logging
+from logging.handlers import RotatingFileHandler
 from local_settings import cKey, cKeySecret, aToken, aTokenSecret
 
+logging.basicConfig(filename='metrics.log',
+                    level=logging.INFO,
+                    format='%(levelname)s %(asctime)s - %(message)s')
 
 def get_random_move(board):
     legal_moves = [move for move in board.legal_moves]
@@ -19,16 +24,16 @@ def game_alive(board):
 
 
 def checkmate_gen():
-    z = 0
+    generated_games = 0
     while True:
+        generated_games += 1
         board = chess.Board()
         while game_alive(board):
             board.push(get_random_move(board))
-            z += 1
 
         if board.is_checkmate():
             if add_to_history(board):
-                print(z)
+                logging.info("{} games generated".format(generated_games))
                 return board
 
 
@@ -37,19 +42,21 @@ def add_to_history(board):
     Returns False if board has already been posted
     Otherwise adds to history and returns True
     """
+    fen = board.fen().split()[0]
     try:
         with open('previous_boards.p', 'rb') as record:
             board_history = pickle.load(record)
     except IOError:  # file does not exist, create new list
         board_history = []
 
-    if board not in board_history:
-        board_history.append(board)
+    if fen not in board_history:
+        board_history.append(fen)
         with open('previous_boards.p', 'wb') as record:
             pickle.dump(board_history, record)
 
         return True
     else:
+        logging.warn("collision for {}".format())
         return False
 
 
@@ -72,8 +79,9 @@ def convert_board_to_utf(board):
         odd tile on even row: black
         odd tile on odd row: white
         """
+
         even_tile = count % 2 == 0
-        even_row  = int(count / 8)
+        even_row  = int(count / 8) % 2 == 0
         if (even_tile and even_row) or not (even_tile or even_row):
             return WHITE_TILE
         else:
@@ -90,9 +98,8 @@ def convert_board_to_utf(board):
         if end_of_line:
             checkmate_utf_list_with_tiles.append('\n')
 
-    last_char = checkmate_utf_list_with_tiles.pop()
-    if last_char != '\n':
-        checkmate_utf_list_with_tiles.append(last_char)
+    if checkmate_utf_list_with_tiles[-1] == '\n':
+        checkmate_utf_list_with_tiles.pop()
 
 
     return "".join(checkmate_utf_list_with_tiles)
@@ -105,7 +112,6 @@ if __name__ == '__main__':
 
     checkmateObj = checkmate_gen()
     fen = checkmateObj.fen().split()[0]
-    tweet_body = "{board}\n{fen}".format(board=convert_board_to_utf(checkmateObj),
-                                         fen=fen)
+    tweet_body = "{}\n{}".format(convert_board_to_utf(checkmateObj), fen)
     print(tweet_body)
     #api.update_status(tweet_body)
